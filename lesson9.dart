abaf1c5f45b7c555773b542388441a079646f38d
Danik <dan_ntu@mail.ru>
2024-08-16 23:41:49 +0500

    switch case

---
+++ lesson9.dart
@@ -0,0 +1,11 @@
+void main(){
+  //final student = 'Danik';
+  final student = 'Mister';
+  switch(student){
+    case 'Danik':
+      print(student);
+      break;
+    default:
+      print("Default student");  
+  }
+}

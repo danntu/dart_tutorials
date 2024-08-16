318ed6d265eac159ab8c724be58301af422822b7
Danik <dan_ntu@mail.ru>
2024-08-16 23:55:25 +0500

    loop for and forin

---
+++ lesson10.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  for (int i = 0; i < 5; i++) 
+  { 
+    print('Hello'+i.toString()); 
+  }  
+
+  final students = ['Dan','Doni'];
+  print(students);
+  for (var student in students) {
+    print(student);
+  }
+
+}
